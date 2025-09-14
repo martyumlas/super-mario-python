27d3f1e211df8fe93ad1b46b60b5b0b7b0dab9e2
martywebdev <martywebdevelopment@gmail.com>
2025-09-14 20:12:29 +0800

    update movements

--- classes/Input.py
+++ classes/Input.py
@@ -15,33 +15,69 @@ class Input:
         self.checkForMouseInput(events)
         self.checkForQuitAndRestartInputEvents(events)
 
+    # def checkForKeyboardInput(self):
+    #     pressedKeys = pygame.key.get_pressed()
+
+    #     if pressedKeys[K_LEFT] or pressedKeys[K_h] and not pressedKeys[K_RIGHT]:
+    #         self.entity.traits["goTrait"].direction = -1
+    #     elif pressedKeys[K_RIGHT] or pressedKeys[K_l] and not pressedKeys[K_LEFT]:
+    #         self.entity.traits["goTrait"].direction = 1
+    #     else:
+    #         self.entity.traits['goTrait'].direction = 0
+
+    #     isJumping = pressedKeys[K_SPACE] or pressedKeys[K_UP] or pressedKeys[K_k]
+    #     self.entity.traits['jumpTrait'].jump(isJumping)
+
+    #     self.entity.traits['goTrait'].boost = pressedKeys[K_LSHIFT]
+
     def checkForKeyboardInput(self):
         pressedKeys = pygame.key.get_pressed()
 
-        if pressedKeys[K_LEFT] or pressedKeys[K_h] and not pressedKeys[K_RIGHT]:
+        # Movement (A/D instead of LEFT/RIGHT)
+        if (pressedKeys[K_a] or pressedKeys[K_h]) and not pressedKeys[K_d]:
             self.entity.traits["goTrait"].direction = -1
-        elif pressedKeys[K_RIGHT] or pressedKeys[K_l] and not pressedKeys[K_LEFT]:
+        elif (pressedKeys[K_d] or pressedKeys[K_l]) and not pressedKeys[K_a]:
             self.entity.traits["goTrait"].direction = 1
         else:
             self.entity.traits['goTrait'].direction = 0
 
-        isJumping = pressedKeys[K_SPACE] or pressedKeys[K_UP] or pressedKeys[K_k]
+        # Jump (W instead of UP)
+        isJumping = pressedKeys[K_w] or pressedKeys[K_SPACE] or pressedKeys[K_k]
         self.entity.traits['jumpTrait'].jump(isJumping)
 
+        # Boost stays the same
         self.entity.traits['goTrait'].boost = pressedKeys[K_LSHIFT]
+
+        # Optional: Crouch with S
+        if pressedKeys[K_s]:
+            if hasattr(self.entity, "crouch"):
+                self.entity.crouch()
+
+    # def checkForMouseInput(self, events):
+    #     mouseX, mouseY = pygame.mouse.get_pos()
+    #     if self.isRightMouseButtonPressed(events):
+    #         self.entity.levelObj.addKoopa(
+    #             mouseY / 32, mouseX / 32 - self.entity.camera.pos.x
+    #         )
+    #         self.entity.levelObj.addGoomba(
+    #             mouseY / 32, mouseX / 32 - self.entity.camera.pos.x
+    #         )
+    #         self.entity.levelObj.addRedMushroom(
+    #             mouseY / 32, mouseX / 32 - self.entity.camera.pos.x
+    #         )
+    #     if self.isLeftMouseButtonPressed(events):
+    #         self.entity.levelObj.addCoin(
+    #             mouseX / 32 - self.entity.camera.pos.x, mouseY / 32
+    #         )
 
     def checkForMouseInput(self, events):
         mouseX, mouseY = pygame.mouse.get_pos()
+
+        # Right click triggers jump
         if self.isRightMouseButtonPressed(events):
-            self.entity.levelObj.addKoopa(
-                mouseY / 32, mouseX / 32 - self.entity.camera.pos.x
-            )
-            self.entity.levelObj.addGoomba(
-                mouseY / 32, mouseX / 32 - self.entity.camera.pos.x
-            )
-            self.entity.levelObj.addRedMushroom(
-                mouseY / 32, mouseX / 32 - self.entity.camera.pos.x
-            )
+            self.entity.traits['jumpTrait'].jump(True)
+
+        # Left click still spawns coins
         if self.isLeftMouseButtonPressed(events):
             self.entity.levelObj.addCoin(
                 mouseX / 32 - self.entity.camera.pos.x, mouseY / 32
@@ -53,7 +89,7 @@ class Input:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN and \
-                (event.key == pygame.K_ESCAPE or event.key == pygame.K_F5):
+                    (event.key == pygame.K_ESCAPE or event.key == pygame.K_F5):
                 self.entity.pause = True
                 self.entity.pauseObj.createBackgroundBlur()
 
@@ -63,8 +99,14 @@ class Input:
     def isRightMouseButtonPressed(self, events):
         return self.checkMouse(events, 3)
 
+    # def checkMouse(self, events, button):
+    #     for e in events:
+    #         if e.type == pygame.MOUSEBUTTONUP and e.button == button:
+    #             return True
+    #     return False
     def checkMouse(self, events, button):
         for e in events:
-            if e.type == pygame.MOUSEBUTTONUP and e.button == button:
+            # Trigger jump/action immediately on press
+            if e.type == pygame.MOUSEBUTTONDOWN and e.button == button:
                 return True
         return False
